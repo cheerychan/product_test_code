dea0765aa647e664a7f92b9ee1adaaa02b335002
cheerychan <cheerychen@glenfly.com>
2023-07-28 11:11:13 +0800

    first commit

---
+++ cmd.c
@@ -0,0 +1,461 @@
+#include "def.h"
+
+extern VIDEO_PCI_PROP video_pci_prop;
+
+DBG_CMD_STRUCT dbg_cmd_table[] = {
+	{"?", do_dbg_help},
+	{"q", do_dbg_quit},
+	{"help", do_dbg_help},
+	{"mmio", do_dbg_mmio},
+	{"ts", do_dbg_ts},
+	{"vcore", do_dbg_voltage},
+	{"sf", do_dbg_flash},
+	{"prog", do_dbg_program_fw},
+	{"dump", do_dbg_dump_fw},
+	{"print", do_dbg_print_info},
+	{"clk", do_dbg_clk},
+	{"memtest", do_dbg_mem_test},
+	{"i2c", do_dbg_i2c_test},
+	{"dp", do_dbg_dp_test},
+	{"efuse", do_dbg_efuse_test},
+	{"ct", do_dbg_cts_test},
+
+};
+
+#define DBG_CMD_TABLE_SIZE (sizeof(dbg_cmd_table) / sizeof(DBG_CMD_STRUCT))
+
+void CToolParserCmd(void)
+{
+	char *line;
+
+	line = (char *)malloc(STRINGBUFFERSIZE);
+	memset(line, 0, STRINGBUFFERSIZE);
+
+	while (1)
+	{
+		EchoWait();
+		CToolGetInput(line, STRINGBUFFERSIZE * sizeof(char));
+		if (ProcessString(line) == 0)
+		{
+			break;
+		}
+	}
+}
+
+int ProcessString(char *buffer) // return 0 if quit, else return 1
+{
+	int bStatus;
+	int i;
+	char delim[] = " \t\n,";
+	char *cmd[MAX_PARAM_COUNT];
+	unsigned char param_count;
+	DBG_CMD_STRUCT *p_cmd_struct;
+
+	for (i = 0; i < MAX_PARAM_COUNT; i++)
+		cmd[i] = NULL;
+
+	cmd[0] = strtok(buffer, delim);
+	if (cmd[0] == NULL)
+		return 1;
+	for (i = 1; i < (MAX_PARAM_COUNT); i++)
+	{
+		cmd[i] = strtok(NULL, delim);
+		if (cmd[0] == NULL)
+			break;
+	}
+
+	param_count = 0;
+	while (cmd[param_count] != NULL)
+	{
+		param_count++;
+	}
+
+	/*Just for debug
+		printf("\n");
+		i = 0;
+		while (cmd[i]!=NULL)
+		{
+			printf("%s ", cmd[i]);
+			i ++;
+		}
+		printf("\n");
+	*/
+	for (i = 0, p_cmd_struct = dbg_cmd_table; i < DBG_CMD_TABLE_SIZE; i++, p_cmd_struct++)
+	{
+		if (strcmp(cmd[0], p_cmd_struct->name) == 0)
+		{
+			bStatus = p_cmd_struct->execute(cmd, param_count);
+			break;
+		}
+	}
+
+	return bStatus;
+}
+
+int do_dbg_mmio(char *cmd[], unsigned int param_count)
+{
+	unsigned int reg_index;
+	unsigned int reg_data;
+	unsigned int reg_start;
+	unsigned int read_num;
+	unsigned int line_index;
+	int i;
+
+	if (param_count == 2) // two parameters. register read.
+	{
+		if (strcmp(cmd[1], "-d") != 0){
+		reg_index = StoH(cmd[1]);
+		reg_data = readl(video_pci_prop.mapped_mmioBase + reg_index);
+		printf("\nRead MMIO %x = 0x%08x\n", reg_index, reg_data);
+	    }	
+	    //for 3588 register dump,write death;
+		else if (strcmp(cmd[1], "-d") == 0)
+		{
+			//dump 0x000-0x29c
+			printf("\nregister 0x000-0x29c\n");
+			reg_start = 0x000;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0xA7; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",i*4);
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			//dump 0x400-0x4a4
+			printf("\nregister 0x400-0x4a4\n");
+			reg_start = 0x400;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x29; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0x400+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			//dump 0x400-0x4a4
+			printf("\nregister 0x800-0x8a4\n");
+			reg_start = 0x800;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x29; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0x800+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			printf("\nregister 0xc00-0xcb4\n");
+			reg_start = 0xc00;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x2D; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0xC00+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			printf("\nregister 0x1000-0x10b4\n");
+			reg_start = 0x1000;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x2D; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0x1000+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			printf("\nregister 0x1400-0x14b4\n");
+			reg_start = 0x1400;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x2D; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0x1400+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}
+			printf("\nregister 0x1800-0x18b4\n");
+			reg_start = 0x1800;
+			printf("         00              04             08             0c        \n");
+			for (i = 0; i <= 0x2D; i++)
+			{
+				if((i * 4)%0x10==0x00){
+                    printf("%04x   ",(0x1800+i*4));
+				}
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+				printf("  0x%08x  ", reg_data);
+				if((i+1)%4==0x00){
+					printf("\n");
+				}
+			}	
+		}
+	}
+	else if (param_count == 3) // register write
+	{
+		reg_index = StoH(cmd[1]);
+		reg_data = StoH(cmd[2]);
+		writel(reg_data, video_pci_prop.mapped_mmioBase + reg_index);
+		printf("\nWrite MMIO %x = 0x%08x Done.\n", reg_index, reg_data);
+	}
+	else if (param_count >= 4)
+	{
+		// ADD MORE REGISTER READ; non-sequence
+		if (strcmp(cmd[1], "-m") == 0)
+		{
+			for (i = 2; i < param_count; i++)
+			{
+				reg_index = StoH(cmd[i]);
+				reg_data = readl(video_pci_prop.mapped_mmioBase + reg_index);
+				printf("\nRead MMIO %x = 0x%08x\n", reg_index, reg_data);
+			}
+		}
+		else if (strcmp(cmd[1], "-b") == 0)
+		{
+				reg_start = StoH(cmd[2]);
+				read_num = StoH(cmd[3]);			
+				for (i = 0; i < read_num; i++)
+				{
+					reg_data = readb(video_pci_prop.mapped_mmioBase + reg_start + i);
+					printf("\nRead MMIO %x = 0x%02x\n", (reg_start + i), reg_data);
+				}
+			
+		}
+		// ADD -W DWORE;sequence
+		else if (strcmp(cmd[1], "-w") == 0)
+		{
+			
+				reg_start = StoH(cmd[2]);
+				read_num = StoH(cmd[3]);
+				for (i = 0; i < read_num; i++)
+				{
+					reg_data = readl(video_pci_prop.mapped_mmioBase + reg_start + i * 4);
+					printf("\nRead MMIO %x = 0x%08x\n", (reg_start + i * 4), reg_data);
+				}
+			
+		}	
+	}
+	else
+	{
+		// printf("\nParse Error!\n");
+		mmio_help_info();
+	}
+
+
+return TRUE;
+}
+
+void mmio_help_info(void)
+{
+	printf("  mmio -m register1 register2 register3...: non-sequence register read.\n");
+	printf("  mmio -b addr num: sequence register read byte.\n");
+	printf("  mmio -w addr num: sequence register read dword\n");
+	printf("  mmio register: register read dword\n");
+	printf("  mmio register [data]: register write dword\n");
+	// printf("  clk -f/F e/E data(MHz)]:change eclk and print eclk and vclk.\n");
+}
+
+int do_dbg_ts(char *cmd[], unsigned int param_count)
+{
+	printf("Temp : %d C\n", dout_to_temp(GetTemperature()) / 1000);
+	return TRUE;
+}
+
+int do_dbg_voltage(char *cmd[], unsigned int param_count)
+{
+	printf("voltage : %d mV\n", (GetVoltage() * 1000 + 1157200) / 1869);
+	return TRUE;
+}
+
+int do_dbg_print_info(char *cmd[], unsigned int param_count)
+{
+	if (video_pci_prop.VenderId == 0x6766 && video_pci_prop.DeviceId == 0x3d02)
+	{
+
+		printf("Technology : 28 nm\n");
+		printf("Die Size : 322 mm*mm\n");
+		printf("Transistors : 445 M\n");
+		printf("ROPS/TMUS : 16/32\n");
+		printf("Shaders : 256 Unified\n");
+		printf("DirectX Support : 11.0\n");
+	}
+	if (video_pci_prop.VenderId == 0x6766 && video_pci_prop.DeviceId == 0x3d00)
+	{
+
+		printf("Technology : 28 nm\n");
+		printf("Die Size : 322 mm*mm\n");
+		printf("Transistors : 445 M\n");
+		printf("ROPS/TMUS : 96/192\n");
+		printf("Shaders : 1536 Unified\n");
+		printf("DirectX Support : 11.0\n");
+	}
+	ts_init();
+	sf_init();
+	read_fw_version();
+	read_PCIe();
+	printf("DDR Version : DDR4\n");
+	read_bitwidth();
+	printf("Memory Clk : %d MHz\n", get_mpll());
+	printf("Elite Clk : %d MHz\n", get_vepll(EPLL_REG));
+	printf("VIDEO Clk : %d MHz\n", get_vepll(VPLL_REG));
+	printf("Temp : %d C\n", dout_to_temp(GetTemperature()) / 1000);
+	printf("voltage : %d mV\n", (GetVoltage() * 1000 + 1157200) / 1869);
+	printf("FAN1 Speed:%d RPM\n", get_fs_speed(1));
+	return true;
+}
+
+int do_dbg_flash(char *cmd[], unsigned int param_count)
+{
+	sf_init();
+	sf_flash_test(param_count, cmd);
+	return TRUE;
+}
+
+int do_dbg_mem_test(char *cmd[], unsigned int param_count)
+{
+	chip_mem_connection_test(param_count, cmd);
+	return TRUE;
+}
+
+int do_dbg_i2c_test(char *cmd[], unsigned int param_count)
+{
+	i2c_prog(param_count, cmd);
+	return TRUE;
+}
+int do_dbg_efuse_test(char *cmd[], unsigned int param_count)
+{
+	// helpinfo();
+	efuse_test(param_count, cmd);
+	return TRUE;
+}
+int do_dbg_dp_test(char *cmd[], unsigned int param_count)
+{
+	chip_mem_connection_test(param_count, cmd);
+	return TRUE;
+}
+int do_dbg_cts_test(char *cmd[], unsigned int param_count)
+{
+	cts_test(param_count, cmd);
+	return TRUE;
+}
+
+int do_dbg_program_fw(char *cmd[], unsigned int param_count)
+{
+	sf_init();
+	if (param_count >= 2)
+	{
+		flash_vbios(cmd[1]);
+	}
+	else
+	{
+		printf("  prog filename: program spi flash.\n");
+	}
+	return TRUE;
+}
+
+int do_dbg_dump_fw(char *cmd[], unsigned int param_count)
+{
+	unsigned int dump_file_size = MAX_VIDEO_ROM_SIZE;
+    sf_init();
+	if (param_count >= 2)
+	{
+		if (param_count == 3)
+		{
+			dump_file_size = atoi(cmd[2]);
+		}
+		else if (param_count == 2)
+		{
+			// read file size from vbios
+			dump_file_size = sf_read_data(FW_FILE_SIZE_OFFSET, FW_FILE_SIZE_LEN);
+			if (dump_file_size == 0)
+			{
+				dump_file_size = MAX_VIDEO_ROM_SIZE;
+				printf("Use default file size! ");
+			}
+			else
+			{
+				printf("Read file size from spi flash! ");
+			}
+		}
+
+		printf("Dump file size = %d\n", dump_file_size);
+		dump_vbios(cmd[1], dump_file_size);
+	}
+	else
+	{
+		printf("  dump filename [filesize(byte)]: dump spi flash file.\n");
+	}
+	return TRUE;
+}
+
+int do_dbg_clk(char *cmd[], unsigned int param_count)
+{
+	clk_prog(param_count, cmd);
+	return TRUE;
+}
+
+int do_dbg_help(char *cmd[], unsigned int param_count)
+{
+	helpinfo();
+	return TRUE;
+}
+
+int do_dbg_quit(char *cmd[], unsigned int param_count)
+{
+	return FALSE;
+}
+
+void helpinfo(void)
+{
+	printf("  help/?     --- print help info\n");
+	printf("  q          --- quit\n");
+	printf("  ts         --- print temperature\n");
+	printf("  vcore      --- print vcore\n");
+	printf("  clk        --- read/write clock\n");
+	printf("  memtest    --- miu test connection\n");
+	printf("  sf         --- flash read and write\n");
+	printf("  prog       --- program rom to flash\n");
+	printf("  dump       --- dump data from flash saved to file\n");
+	printf("  mmio       --- read/write register; -m read more registers\n");
+	printf("  i2c        --- i2c read/write data\n");
+	printf("  print      --- print all information(pcie info/mem info/vcore/clk/temp/fs)\n");
+	printf("  efuse      --- efuse hdcp key1.4/2.2 function\n");
+	printf("  ct         --- only for linux; need uma and test graphics card\n");
+}
+
+char GetKey(void)
+{
+	char key;
+	/*#ifdef __dos__
+		key = getch();
+	#endif
+	#ifdef __ubuntu__*/
+	key = getchar();
+	//#endif
+	// if (key == 0)
+	//	key = (0x80 | getch());
+	return key;
+}
---
+++ main.c
@@ -0,0 +1,134 @@
+#include "def.h"
+
+
+VIDEO_PCI_PROP video_pci_prop;
+configuration config;
+
+static int handler(void* user, const char* section, const char* name, const char* value)
+{
+    configuration* pconfig = (configuration*)user;
+
+    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("id", "vendorid")) 
+    {
+        pconfig->id.vendorid = htoi(value);
+    }
+    else if(MATCH("id", "deviceid"))
+    {
+        pconfig->id.deviceid = htoi(value);
+    }
+    else if(MATCH("addr", "mmiobase"))
+    {
+        pconfig->addr.mmiobase = htoi(value);
+    }  
+     else if(MATCH("i2c", "crt"))
+    {
+        pconfig->i2c.serialport0 = htoi(value);
+    } 
+    else if(MATCH("i2c", "hdmi0"))
+    {
+        pconfig->i2c.serialport1 = htoi(value);
+    } 
+    else if(MATCH("i2c", "hdmi1"))
+    {
+        pconfig->i2c.serialport2 = htoi(value);
+    }
+    else if(MATCH("i2c", "i2cdelay"))
+    {
+        pconfig->i2c.i2c_delay = atoi(value);
+    }        
+    else
+    {
+        return 0;  /* unknown section/name, error */
+    }
+    return 1;
+}
+
+void init_config_parameters(configuration* pconfig)
+{
+    pconfig->i2c.serialport0 = 0x89f8;
+    pconfig->i2c.serialport1 = 0x88a0;
+    pconfig->i2c.serialport2 = 0x88aa;
+    pconfig->id.vendorid = 0x6766;
+    pconfig->id.deviceid = 0x3d00;
+    pconfig->i2c.i2c_delay = 400;
+    pconfig->addr.mmiobase = 0xfed60000;
+}
+
+int main(int argc, char *argv[])
+{
+    int id_index = 16;
+    int no_config_file = TRUE;
+    char* config_file_name = "glendbg.ini";
+    struct stat t_stat;
+    stat("glenfly_tool_debug", &t_stat);
+    printf("Version date is: %s", ctime(&t_stat.st_mtime));
+    unsigned int data;
+    unsigned int data1;
+    
+    init_config_parameters(&config);
+
+    if(argc == 2)
+    {
+        config_file_name = argv[1];
+    }
+
+    if(ini_parse(config_file_name, handler, &config)<0) 
+    {
+        printf("Can't load %s\n",config_file_name);
+        //printf("Load chip id default : vendorid = 0x%x, deviceid = 0x%x\n", config.id.vendorid, config.id.deviceid);
+    }
+    else
+    {
+        no_config_file = FALSE;
+        printf("Load chip id from file : vendorid = 0x%x, id.deviceid = 0x%x\n", config.id.vendorid, config.id.deviceid);
+    }
+
+    //loop find device id 3d00-3d0e
+    do
+    {      
+        if(find_Base_Addr(config))
+        {
+            printf("Load chip id : vendorid = 0x%x, deviceid = 0x%x\n", config.id.vendorid, config.id.deviceid);
+            break;
+        }
+        else if(!no_config_file)
+        {
+            printf("Can't find device in PCIE!\n");  
+            printf("Load mmiobase from file: mmiobase = 0x%lx\n", config.addr.mmiobase); 
+            video_pci_prop.MmioBase = config.addr.mmiobase;  
+        }
+
+        if(id_index==1)
+        {
+            //load mmio base from config.ini
+            printf("Can't read mmio base address from pcie.\n");
+            printf("Load default base address: mmiobase = 0x%lx\n", config.addr.mmiobase);
+            video_pci_prop.MmioBase = config.addr.mmiobase;
+        }
+        config.id.deviceid++;
+    }while (id_index-- && no_config_file);
+
+  
+#ifdef __ubuntu__
+    if(!map_to_system_memory(video_pci_prop.MmioBase))
+    {
+        return FALSE;
+    }
+#endif
+
+#ifdef __dos__
+    video_pci_prop.mapped_mmioBase = video_pci_prop.MmioBase;
+#endif
+    data = HdcpEfuseReadOneDWord(0x77);
+    HdcpEfuseReadssiddDWord(data);
+    data1 = HdcpEfuseReadOneDWord(0x7A);
+    data = HdcpEfuseReadOneDWord(0x7B);
+    HdcpEfuseReadLWord(data1, data);
+    HdcpEfuseReadWCDWord(data);
+    //helpinfo();
+    //CToolParserCmd();
+	return 1;
+}
+
+			
